bc1ef1eb1a9e4ffc8ef7412326da7ae21bbaaf90
jonathan.gobiel <jonathan.gobiel@bytedance.com>
2024-12-09 22:38:10 +0700

    feat: supabase init

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,13 @@
-export function GET() {
-    return Response.json({
-        data: []
-    })
+import createResponse from "../(functions)/create-response"
+import { supabase } from "../(functions)/db"
+
+export async function GET() {
+    const users = await supabase.from("user").select()
+    return Response.json(createResponse({
+        type : "success",
+        status : 200,
+         payload: users.data
+    }))
 }
 
 export function POST() {
