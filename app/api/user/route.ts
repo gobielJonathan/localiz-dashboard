cbc6f84486d7c91c4cf0c552430961fc3b02af21
jonathan.gobiel <jonathan.gobiel@bytedance.com>
2024-12-13 10:49:36 +0700

    chore: add teams response in user api

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,5 @@
 import createResponse from "@/function/create-response";
+import { supabase } from "@/function/db";
 import getAuthUser from "@/repository/auth/get-auth-user";
 import { asyncTryCatch } from "@/utils/try-catch";
 
@@ -13,11 +14,14 @@ export async function GET(_req: Request) {
         })
     }
 
+    const  teams = await supabase.from("team").select("id, dashboard ( id , name ) ").eq("user_id", user?.user.id || "")
+    
     return createResponse({
         type: "success",
         payload: {
             email: user?.user.email,
-            id: user?.user.id
+            id: user?.user.id,
+            teams: teams.data
         },
         status: 200
     })
