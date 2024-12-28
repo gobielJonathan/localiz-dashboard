87330efe93f3f28a169513a74ee522d9dae84d34
jonathan.gobiel <jonathan.gobiel@bytedance.com>
2024-12-28 19:49:05 +0700

    feat: add security middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,24 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
+import arcjet, { createMiddleware, detectBot, fixedWindow } from '@arcjet/next';
+
 import { supabase } from './function/db';
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+  characteristics: ['userId'],
+  rules: [
+    fixedWindow({
+      mode: 'LIVE', // will block requests. Use "DRY_RUN" to log only
+      window: '60s', // 60 second fixed window
+      max: 100, // allow a maximum of 100 requests
+    }),
+    detectBot({ mode: 'LIVE', allow: [] }),
+  ],
+});
+
+export default createMiddleware(aj);
 
 export async function middleware(req: NextRequest) {
   const cookiesStore = await cookies();
@@ -22,6 +39,21 @@ export async function middleware(req: NextRequest) {
       new URL('/login?cb=' + req.nextUrl.href, req.url),
       { status: 301 },
     );
+  }
+
+  if (req.nextUrl.pathname.startsWith('/api')) {
+    console.log('checking');
+    const decision = await aj.protect(req, {
+      userId: user.user?.id ?? '',
+      requested: 5,
+    }); // Deduct 5 tokens from the bucket
+
+    if (decision.isDenied()) {
+      return NextResponse.json(
+        { error: 'Too Many Requests', reason: decision.reason },
+        { status: 429 },
+      );
+    }
   }
 
   // Allow the request to continue if valid
